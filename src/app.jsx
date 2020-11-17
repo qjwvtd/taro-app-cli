ceb9d30b41c411b192e15d9236f08ead1766e901
张小军 <zhang.xiaojun@ttwx365.com>
2020-11-17 13:25:31 +0800

    2020-11-17,13:25,添加小程序授权代码

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 //taro-ui样式文件必须在入口文件最顶部引入
 import 'taro-ui/dist/style/index.scss';
-import React, {  } from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'mobx-react';
 import counterStore from './common/store/counter';
 
@@ -12,6 +12,22 @@ const store = {
 };
 
 function App({children}){
+    useEffect(() => {
+        //小程序授权
+        wx.getSetting({
+            success(res) {
+                if (!res.authSetting['scope.record']) {
+                    wx.authorize({
+                        scope: 'scope.record',
+                        success () {
+                            // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+                            wx.startRecord();
+                        }
+                    })
+                }
+            }
+        })
+    },[]);
     return <Provider store={store}>
         {children}
     </Provider>;
